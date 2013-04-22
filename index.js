4d401a7f3e098002d3de4445812d873f4040a784
trevorah <a.trevorah@gmail.com>
2013-04-22 16:35:10 +0100

    event emitters are lovely

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
-var prices = require('./prices.json');
+var seedPrices = require('./prices.json');
+var events = require('events');
 
-var getQuote = function(code) {
-	var rate = prices[code];
-	var modifier = Math.random();
-	return rate + modifier;
-};
+var eventEmitter = new events.EventEmitter();
 
-var getUpdates = function(code, callback) {
-	var id = setInterval(function() {
-		var quote = getQuote(code);
-		callback(quote);
-	}, 1000);
-	return id;
-};
+var updateCurrencyPairAtInterval = function(currencyPair, seedPrice, spread) {
+	setTimeout(function() {
+		setInterval(function() {			
+			var price = seedPrice + (Math.random() - 0.5)/1000;
+			var bid = price - spread / 2;
+			var offer = price + spread / 2;
 
-module.exports.getQuote = getQuote;
+			bid =  Math.floor(bid * 1000000) / 1000000;
+			offer = Math.floor(offer * 1000000) / 1000000;
+			
+			var event = {
+				'currencyPair' : currencyPair,
+				'bid':bid,
+				'offer':offer
+			}
+
+			eventEmitter.emit(currencyPair, event);
+			eventEmitter.emit('all', event);
+		}, Math.floor(Math.random() * 10) + 1000);
+	}, Math.floor(Math.random() * 5000));
+}
+
+for(var currencyPair in seedPrices) {
+	var seedPrice = seedPrices[currencyPair];
+	var pointOnePercent = (seedPrice) / 1000;
+	updateCurrencyPairAtInterval(currencyPair, seedPrice, pointOnePercent);
+}
+
+module.exports = eventEmitter;
--- test/test.js
+++
@@ -1,7 +0,0 @@
-var assert = require('assert');
-var money = require('../index.js');
-describe('getQuote()', function(){
-    it('should return something', function(){
-      assert( money.getQuote('USDVND') > 100 );
-    })
-})
